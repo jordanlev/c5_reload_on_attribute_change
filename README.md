a42e62e0f03a5ad1aa06c3c62101c2723d87ba9f
Jordan Lev <github@jordanlev.com>
2012-04-28 01:43:20 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ c5_reload_on_attribute_change
 =============================
 
 Reloads page after attributes are saved, so users see immediate feedback on page types that use attributes for layout/design options.
+
+## Requirements
+Probably only works with Concrete5.5+
+
+## Installation
+Drop the `reload_on_attribute_change.php` file into your site's top-level `elements` directory.
+
+## Usage
+Add this to the top of any page type templates you want this functionality on:
+
+    Loader::element('reload_on_attribute_change');
+
+(A good place to add it is under the `include('elements/header.php');` line.)
---
+++ elements/reload_on_attribute_change.php
@@ -0,0 +1,36 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+
+$c = Page::getCurrentPage();
+$cp = new Permissions($c);
+
+if (is_object($cp) && $cp->canAdminPage()) {
+	$js = <<<EOT
+	//Reload page when user updates custom attributes / properties
+	// by overriding the ccmAlert.hud function (which is called by C5 after properties are saved).
+	// See concrete/js/ccm_app/legacy_dialog.js for ccmAlert.hud definition,
+	// see concrete/elements/collection_metadata.php for where it's called (look for '$("#ccmMetadataForm").ajaxForm({')
+	$(window).load(function() { //window.load happens after document.ready, so do this to ensure we're firing after ccmAlert is originally defined by C5
+		alert('hi');
+		if (typeof ccmAlert !== 'undefined') {
+			alert('ho');
+			ccmAlert._hud = ccmAlert.hud;
+			ccmAlert.hud = function(message, time, icon, title) {
+				message = (typeof message !== 'undefined') ? message : null;
+				time = (typeof time !== 'undefined') ? time : null;
+				icon = (typeof icon !== 'undefined') ? icon : null;
+				title = (typeof title !== 'undefined') ? title : null;
+
+				//Check for signature indicating that properties were just saved
+				if ((message == ccmi18n.savePropertiesMsg) && (time == 2000) && (icon == 'success') && (title == ccmi18n.properties)) {
+					window.location.reload(true); //pass true to force get (reload from server, not browser cache)
+				//Not a property save -- pass args through to the original hud() function
+				} else {
+					ccmAlert._hud(message, title, icon, title);
+				}
+			}
+		}
+	});
+EOT;
+
+	$this->addFooterItem(Loader::helper('html')->script($js));
+}
